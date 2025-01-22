d500571201441a5729bd6db3c9c39d280799f1c3
Akansha <akanshamore21@gmail.com>
2025-01-22 13:42:57 +0530

    added login and register backend apis

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/products", productsRoutes);
+// app.use("/api/cart", cartRoutes);
+
+module.exports = app;
